05bfb0eac36282de7c8d5b92813e21cf5568fcd8
brancobruyneel <43569324+brancobruyneel@users.noreply.github.com>
2025-05-21 14:19:47 +0200

    feat: 1password cli

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,10 +2,14 @@ let
   makboek = "ssh-ed25519 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGFqUkC4sOTUEsNfv6ueUUWG1juVvdFoEt8C3v31Z6ad";
   branco = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPBDTzMseGTUyf73n/To64ATwhVfp1mkkhcwzFrw4+uj";
 
-  systems = [makboek];
-  users = [branco];
+  systems = [ makboek ];
+  users = [ branco ];
 
-  work = [makboek branco];
-in {
+  work = [
+    makboek
+    branco
+  ];
+in
+{
   "git/work.age".publicKeys = work;
 }
