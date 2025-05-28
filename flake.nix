aa58721de43c7e7f51729af9946dbbaac8635061
brancobruyneel <43569324+brancobruyneel@users.noreply.github.com>
2025-05-28 10:28:20 +0200

    fix(flake): homebrew-nix override

--- flake.nix
+++ flake.nix
@@ -12,7 +12,6 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
-    nix-homebrew.inputs.nixpkgs.follows = "nixpkgs";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
