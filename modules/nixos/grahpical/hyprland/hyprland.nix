3416ff64e14e652c5edb5e8ce13de076ecdec437
brancobruyneel <43569324+brancobruyneel@users.noreply.github.com>
2025-08-04 11:02:55 +0200

    feat(hyprland): add workspace rules

--- modules/nixos/grahpical/hyprland/hyprland.nix
+++ modules/nixos/grahpical/hyprland/hyprland.nix
@@ -139,6 +139,20 @@
     ", XF86AudioPrev, exec, playerctl previous"
   ];
 
+  workspace = [
+    "1, monitor:DP-2, default:true"
+    "2, monitor:DP-2, default:true"
+    "3, monitor:DP-2, default:true"
+    "4, monitor:DP-2, default:true"
+    "5, monitor:DP-2, default:true"
+
+    "6, monitor:DP-1, default:true"
+    "7, monitor:DP-1, default:true"
+    "8, monitor:DP-1, default:true"
+    "9, monitor:DP-1, default:true"
+    "0, monitor:DP-1, default:true"
+  ];
+
   bindm = [
     "$mod, mouse:272, movewindow"
     "$mod, mouse:273, resizewindow"
